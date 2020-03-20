512df1fabe0876074535a1042daf3ee2006ea3aa
steven yang <stevenyang@stevendeAir-3.fios-router.home>
2020-03-20 17:22:49 -0400

    assignment updated

--- lecture3/solution/Fibonacci.py
+++ lecture3/solution/Fibonacci.py
@@ -1,9 +1,11 @@
 def fibonacci(n):
     if n <= 1:
         return 1
-#    elif n == 3:
-#        return 2
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 3
     else:
         return fibonacci(n-1)*fibonacci(n-2)*fibonacci(n-3)
 
-print(fibonacci(5))
+print(fibonacci(4))
